b6fce25e920203c62008e694a686e43995d8c40c
WF <opibarua1111@gmail.com>
2022-01-03 12:10:23 +0600

    services GET POST DELETE UPDATE and reviews, users, purchases

--- index.js
+++ index.js
@@ -21,14 +21,168 @@ async function run() {
         const productsCollection = database.collection('services');
         const purchaseCollection = database.collection('purcheases');
         const reviewsCollection = database.collection('reviews');
-        const userCollection = database.collection('users');
+        const usersCollection = database.collection('users');
 
-        //GET API
+        // get services data
         app.get('/services', async (req, res) => {
             const cursor = productsCollection.find({});
             const products = await cursor.toArray();
-            res.send(products);
+            res.send(products)
+        });
+        // post service data
+
+        app.post('/services', async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.json(result);
+        });
+        // get single service
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const products = await productsCollection.findOne(query);
+            res.json(products)
+        });
+
+        // delete single service
+
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await productsCollection.deleteOne(query);
+            res.json(result)
+        });
+
+
+
+        // Get purchase data
+
+        app.get('/my-orders', async (req, res) => {
+            const cursor = purchaseCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders)
+        });
+
+        // Delete purchase data
+
+        app.delete('/my-orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await purchaseCollection.deleteOne(query);
+            res.json(result)
+        });
+
+        // Get purchase data by email
+
+        app.get('/my-orders/:email', async (req, res) => {
+            const email = req.params.email;
+            const orders = await purchaseCollection.find({ email: email }).toArray();
+            res.json(orders)
+        });
+
+        // Update status to Shipped
+
+        app.put('/udpate/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    status: "Shipped"
+                },
+            };
+            const result = await purchaseCollection.updateOne(filter, updatedDoc, options);
+
+            res.json(result);
         })
+
+        // post purchase data
+
+        app.post('/purchase', async (req, res) => {
+            const purchase = req.body;
+            const result = await purchaseCollection.insertOne(purchase);
+            res.json(result);
+        });
+
+        // get review data
+
+        app.get('/review', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews)
+        });
+
+        // post review data
+
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+            res.json(result);
+        });
+
+
+
+        // get user data
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false
+            if (user?.role === 'admin') {
+                isAdmin = true
+            }
+            res.json({ admin: isAdmin });
+        });
+
+        // post user 
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result)
+        });
+
+        // get user
+
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users)
+        });
+
+
+
+        // upsert user 
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        // put role user to admin
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(403).json({ message: 'You do not have acces to make admin' })
+            }
+        });
 
     }
     finally {
